412c6a3ce5ad818275f8316fdc10bb685f0b6f1a
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-09-27 15:24:21 -0400

    PaymentDetails et ExecutePayment

--- classes/paypal/adaptativepayments/executepayment.php
+++ classes/paypal/adaptativepayments/executepayment.php
@@ -1,7 +1,19 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @link https://cms.paypal.com/ca/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_api_APExecutePaymentAPI
+ * @author Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com> 
  */
+class ExecutePayment extends PayPal_AdaptativePayments {
+
+    protected function request_rules() {
+        
+    }
+
+    protected function response_rules() {
+        
+    }
+
+}
+
 ?>
--- classes/paypal/adaptativepayments/paymentdetails.php
+++ classes/paypal/adaptativepayments/paymentdetails.php
@@ -1,7 +1,28 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @link https://cms.paypal.com/ca/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_api_APPaymentDetails 
  */
+class PaymentDetails extends PayPal_AdaptativePayments {
+
+    protected function request_rules() {
+        return array(
+            'payKey' => array(
+                array('not_empty')
+            ),
+            'transactionId' => array(
+                array('not_empty')
+            ),
+            'trackingId' => array(
+                array('not_empty')
+            )
+        );
+    }
+
+    protected function response_rules() {
+        
+    }
+
+}
+
 ?>
