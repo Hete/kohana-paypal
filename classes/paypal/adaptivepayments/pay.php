1db019fedfab51b5a8a019b1f82cd75ba9956b47
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-10-01 16:39:27 -0400

    Fix dans les validations

--- classes/paypal/adaptivepayments/pay.php
+++ classes/paypal/adaptivepayments/pay.php
@@ -11,20 +11,20 @@ class PayPal_AdaptivePayments_Pay extends PayPal_AdaptivePayments {
     protected function request_rules() {
         return array(
             'actionType' => array(
-                'not_empty',
-                'regex' => array(':value', '^'.implode('|', PayPal_AdaptivePayments_Pay::$ACTION_TYPE).'$')
+                array('not_empty'),
+                //array('regex', array(':value', '^'.implode('|', PayPal_AdaptivePayments_Pay::$ACTION_TYPE).'$')),
             ),
             'currencyCode' => array(
-                'not_empty',
-                'regex' => array(':value', '^'.implode('|', PayPal_AdaptivePayments::$CURRENCIES).'$')
+                array('not_empty'),
+                //array('regex', array(':value', '^'.implode('|', PayPal_AdaptivePayments::$CURRENCIES).'$')),
             ),            
             'cancelUrl' => array(
-                'not_empty',
-                'url',
+                array('not_empty'),
+                array('url'),
             ),
             'returnUrl' => array(
-                'not_empty',
-                'url',
+                array('not_empty'),
+                array('url'),
             ),            
         );
     }
