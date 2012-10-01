4fefa1fc010b5f2efcebc32a2877e06aaaab24b8
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-10-01 16:14:37 -0400

    Liste vides pour le GetBasicPersonalData

--- classes/paypal/permissions/getbasicpersonaldata.php
+++ classes/paypal/permissions/getbasicpersonaldata.php
@@ -3,11 +3,12 @@
 class PayPal_Permissions_GetBasicPersonalData extends PayPal_Permissions {
     
     protected function request_rules() {
-        
+        return array();
     }
 
     protected function response_rules() {
-        
+                return array();
+
     }
 }
 
