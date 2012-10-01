93186bd866fca5036a769084bb500efa7ee7ff32
Quentin <Quentin@quentin.local>
2012-10-01 12:47:32 -0400

    CancelPreAproval + Maj SetPreAproval

--- classes/paypal/adaptivepayments/cancelpreapproval.php
+++ classes/paypal/adaptivepayments/cancelpreapproval.php
@@ -4,4 +4,17 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+class PayPal_AdaptativePayments_CancelPreapproval extends PayPal_AdaptivePayments{
+    
+protected function request_rules() {
+        return array(
+            'preapprovalKey' => array(
+                array('not_empty')
+            )
+        );
+    }
+protected function response_rules() {
+        
+    }
+}
 ?>
--- classes/paypal/adaptivepayments/preapprovaldetails.php
+++ classes/paypal/adaptivepayments/preapprovaldetails.php
@@ -28,79 +28,80 @@ class PayPal_AdaptativePayments_PreapprovalDetails extends PayPal_AdaptativePaym
     }
 
     protected function response_rules() {
-        return array(
-            //PreapprovalDetailsResponse Fields
-            'addresslist' => array(
-                array('not_empty')                
-            ),
-            'approved' => array(
-                array('boolean')
-            ),
-            'cancelUrl' => array(
-                array('not_empty')                
-            ),
-            'curPayments' => array(
-                array('not_empty')
-            ),
-            'curPaymentsAmount' => array(
-                array('not_empty')
-            ),
-            'curPeriodAttempts' => array(
-                array('not_empty')
-            ),
-            'curPeriodEndingDate' => array(
-                array('not_empty')
-            ),
-            'currencyCode' => array(
-              array('not_empty'),
-              array('regex', array(':value', '^' .'^'.explode('|', Paypal::$CURRENCIES).'$'))
-            ),
-            'dateOfMonth' => array(
-                array('range' , array (':value', array('0', '30'))),
-                array('numeric'),
-                array('not_empty')
-            ),
-            'dayOfWeek' => array(
-                array('not_empty'),
-                array('regex', ':value', '^'.explode('|', Paypal::$DAYS_OF_WEEK) . '*')
-            ),
-            'endingDate' => array(
-                array('not_empty'),
-                array('date', array(":value", PayPal::DATE_FORMAT)),                
-            ),
-            'ipnNotificationUrl' => array(
-                array('not_empty'),
-                array('url'),
-            ),
-            'maxAmountPerPayment' => array(
-                array('not_empty'),
-                array('numeric'),
-            ),
-            'maxNumberOfPayments' => array(
-                array('not_empty'),
-                array('digit'),
-            ),
-            'maxNumberOfPaymentsPerPeriod' => array(
-                array('digit'),
-                array('not_empty'),
-                ),
-            'maxTotalAmountOfAllPayments' => array(
-                array('not_empty'),
-                array('numeric'),
-            ),
-            'memo' => array(
-                array('not_empty'),
-                ),
-            'paymentPeriod' => array(
-                array('not_empty'),
-                array('regex', array(':value', '^' .'^'.explode('|', Paypal::$PAYMENT_PERIODS).'$'))
-            ),
-            'pinType' => array(
-                array('not_empty'),
-                array('regex', array(':value', '^' .'^'.explode('|', Paypal::$PERSONAL_IDENTIFICATION_NUMBER).'$'))
-            ),
-            
-        );
+//        return array(
+//            //PreapprovalDetailsResponse Fields
+//            //TODO ADDRESS
+//            'addresslist' => array(
+//                array('not_empty')                
+//            ),
+//            'approved' => array(
+//                array('boolean')
+//            ),
+//            'cancelUrl' => array(
+//                array('not_empty')                
+//            ),
+//            'curPayments' => array(
+//                array('not_empty')
+//            ),
+//            'curPaymentsAmount' => array(
+//                array('not_empty')
+//            ),
+//            'curPeriodAttempts' => array(
+//                array('not_empty')
+//            ),
+//            'curPeriodEndingDate' => array(
+//                array('not_empty')
+//            ),
+//            'currencyCode' => array(
+//              array('not_empty'),
+//              array('regex', array(':value', '^' .'^'.explode('|', Paypal::$CURRENCIES).'$'))
+//            ),
+//            'dateOfMonth' => array(
+//                array('range' , array (':value', array('0', '30'))),
+//                array('numeric'),
+//                array('not_empty')
+//            ),
+//            'dayOfWeek' => array(
+//                array('not_empty'),
+//                array('regex', ':value', '^'.explode('|', Paypal::$DAYS_OF_WEEK) . '*')
+//            ),
+//            'endingDate' => array(
+//                array('not_empty'),
+//                array('date', array(":value", PayPal::DATE_FORMAT)),                
+//            ),
+//            'ipnNotificationUrl' => array(
+//                array('not_empty'),
+//                array('url'),
+//            ),
+//            'maxAmountPerPayment' => array(
+//                array('not_empty'),
+//                array('numeric'),
+//            ),
+//            'maxNumberOfPayments' => array(
+//                array('not_empty'),
+//                array('digit'),
+//            ),
+//            'maxNumberOfPaymentsPerPeriod' => array(
+//                array('digit'),
+//                array('not_empty'),
+//                ),
+//            'maxTotalAmountOfAllPayments' => array(
+//                array('not_empty'),
+//                array('numeric'),
+//            ),
+//            'memo' => array(
+//                array('not_empty'),
+//                ),
+//            'paymentPeriod' => array(
+//                array('not_empty'),
+//                array('regex', array(':value', '^' .'^'.explode('|', Paypal::$PAYMENT_PERIODS).'$'))
+//            ),
+//            'pinType' => array(
+//                array('not_empty'),
+//                array('regex', array(':value', '^' .'^'.explode('|', Paypal::$PERSONAL_IDENTIFICATION_NUMBER).'$'))
+//            ),
+//            
+//        );
     }
 
 }
