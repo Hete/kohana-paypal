8063a0732372ff67af9d075615eeb438d5ffa920
Christian Martinez <chr1st1anmart1nez@hotmail.com>
2012-12-10 14:17:10 -0500

    Fix pour DoExpressCheckout,,.,

--- classes/paypal/expresscheckout/getexpresscheckoutdetails.php
+++ classes/paypal/expresscheckout/getexpresscheckoutdetails.php
@@ -3,6 +3,9 @@
 defined('SYSPATH') or die('No direct script access.');
 
 class PayPal_ExpressCheckout_GetExpressCheckoutDetails extends Kohana_PayPal_ExpressCheckout_GetExpressCheckoutDetails {
+  
+  
+    
     
 }
 
