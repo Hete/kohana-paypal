605213be8084e08872132620fd9c19c8c56a09d4
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-12-16 17:28:48 -0500

    New tests for adaptivepayment and request/response.

--- tests/paypal.php
+++ tests/paypal.php
@@ -6,28 +6,32 @@ defined('SYSPATH') or die('No direct script access.');
  * General tests for the PayPal module.
  */
 class PayPal_Test extends Unittest_TestCase {
-    
+
+    public function setUp() {
+        parent::setUp();
+        Request::$initial = "";
+    }
+
     /**
      * Various assertions about a PayPal request.
      */
     public function test_request() {
-        
+
         $request = PayPal::factory("AdaptivePayments_Pay");
 
         // API url must be a valid url
         $this->assertTrue(Valid::url($request->api_url()));
-        
+
         // Param is an alias for post
         $this->assertEquals($request->param(), $request->post());
-        
+
         // Curl key exists
         $this->assertNotNull($request->config("curl"));
-        
+
         // Curl options is at least an array
         $this->assertTrue(Arr::is_array($request->config("curl.options")));
-        
     }
-    
+
     /**
      * Test a PayPal response. PayPal request must be correct in order to 
      * test responses.
@@ -35,15 +39,27 @@ class PayPal_Test extends Unittest_TestCase {
      */
     public function test_response() {
 
-        $request = PayPal::factory("AdaptivePayments_Pay");
+        $data = array(
+            "actionType" => PayPal_AdaptivePayments_Pay::PAY,
+            "cancelUrl" => "http://www.x.com",
+            "returnUrl" => "http://www.x.com",
+            "currencyCode" => "CAD",
+            "receiverList.receiver(0).email" => "foo@gmail.com",
+            "receiverList.receiver(0).amount" => 44.50,
+        );
 
-        $this->assertTrue(Valid::url($request->api_url()));
-        
-        
-        
-        
+        $response = PayPal::factory("AdaptivePayments_Pay", $data)->execute();
+
+        $this->assertInstanceOf("Response_PayPal", $response);
+
+        $this->assertInstanceOf("Validation", $response);
+
+        // Redirect url, if not empty, must be a valid url
+        if (Valid::not_empty($response->redirect_url)) {
+            $this->assertTrue(Valid::url($response->redirect_url));
+        }
     }
-    
+
 }
 
 ?>
--- tests/paypal/adaptivepayments/pay.php
+++ tests/paypal/adaptivepayments/pay.php
@@ -7,6 +7,54 @@ class PayPal_AdaptivePayments_Pay_Test extends Unittest_TestCase {
         Request::$initial = '';
     }
 
+    /**
+     * @expectedException PayPal_Exception
+     */
+    public function test_invalid_request_wrong_parameter() {
+        $data = array(
+            "actionType" => PayPal_AdaptivePayments_Pay::PAY,
+            "cancelUrl" => "http://www.x.com",
+            "returnUrl" => "http://www.x.com",
+            "currencyCode" => "CAD",
+            "receiverList.receiver(0).email" => "foogmail.com", // Invalid email
+            "receiverList.receiver(0).amount" => 44.50,
+        );
+
+        PayPal::factory("AdaptivePayments_Pay", $data)->execute();
+    }
+
+    /**
+     * @expectedException PayPal_Exception
+     */
+    public function test_invalid_request_wrong_action_type() {
+        $data = array(
+            "actionType" => "FOO!",
+            "cancelUrl" => "http://www.x.com",
+            "returnUrl" => "http://www.x.com",
+            "currencyCode" => "CAD",
+            "receiverList.receiver(0).email" => "foo@gmail.com", // Invalid email
+            "receiverList.receiver(0).amount" => 44.50,
+        );
+
+        PayPal::factory("AdaptivePayments_Pay", $data)->execute();
+    }
+
+    /**
+     * @expectedException PayPal_Exception
+     */
+    public function test_invalid_request_missing_parameter() {
+        $data = array(
+            "actionType" => PayPal_AdaptivePayments_Pay::PAY,
+            "cancelUrl" => "http://www.x.com",
+            "returnUrl" => "http://www.x.com",
+            "currencyCode" => "CAD",
+            "receiverList.receiver(0).email" => "foo@gmail.com",
+                // amount is missing
+        );
+
+        PayPal::factory("AdaptivePayments_Pay", $data)->execute();
+    }
+
     public function test_complete_request() {
 
         $data = array(
@@ -18,7 +66,11 @@ class PayPal_AdaptivePayments_Pay_Test extends Unittest_TestCase {
             "receiverList.receiver(0).amount" => 44.50,
         );
 
-        PayPal::factory("AdaptivePayments_Pay", $data)->execute();
+        $response = PayPal::factory("AdaptivePayments_Pay", $data)->execute();
+
+        $this->assertArrayHasKey("payKey", $response->data());
+
+        $this->assertTrue(Valid::url($response->redirect_url));
     }
 
 }
