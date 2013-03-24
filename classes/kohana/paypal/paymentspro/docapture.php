e98262a2e5c56af96cd78f8cddec18a452cb390b
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-03-24 19:11:03 -0400

    Added rules and constants for DoCapture.

--- classes/kohana/paypal/paymentspro/docapture.php
+++ classes/kohana/paypal/paymentspro/docapture.php
@@ -13,9 +13,25 @@ defined('SYSPATH') or die('No direct script access.');
  */
 class Kohana_PayPal_PaymentsPro_DoCapture extends PayPal_PaymentsPro {
 
-    protected function rules() {
+    const COMPLETE = "Complete", NOT_COMPLETE = "NotComplete";
 
+    public static $COMPLETE_TYPES = array(
+        "Complete", "NotComplete"
+    );
+
+    public function rules() {
         return array(
+            "AUTHORIZATIONID" => array(
+                array("not_empty")
+            ),
+            "COMPLETETYPE" => array(
+                array("not_empty"),
+                array("PayPal_Valid::contained", array(":value", static::$COMPLETE_TYPES))
+            ),
+            "AMT" => array(
+                array("not_empty"),
+                array("PayPal_Valid::numeric")
+            )
         );
     }
 
